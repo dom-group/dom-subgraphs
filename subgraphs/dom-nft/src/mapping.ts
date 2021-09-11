81ddd9c5f9f0d989af6e6791e0852c5f09eb55da
ycl <yauquenlay@gmail.com>
2021-09-11 10:12:35 +0800

    domnft subgraph

---
+++ subgraphs/dom-nft/src/mapping.ts
@@ -0,0 +1,27 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  Mint,
+  Transfer
+} from "../generated/Domino/Domino"
+import { DominoInfo } from "../generated/schema"
+
+export function handleMint(event: Mint): void {
+  let id = event.params.tokenId.toString();
+  let domino = new DominoInfo(id);
+  domino.minter = event.params.minter;
+  domino.holder = event.params.holder;
+  domino.tokenURI = event.params._tokenURI;
+  domino.tag = event.params._tag;
+  domino.cover = event.params._cover;
+  domino.mintTime = event.block.timestamp;
+  domino.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  let id = event.params.tokenId.toString();
+  let domino = DominoInfo.load(id);
+  if(domino!=null) {
+    domino.holder = event.params.to;
+    domino.save();
+  }
+}
